47bd9d8b1c866604b408b0767569870c348fd59a
Moiz Khan <118538665+Moizgith@users.noreply.github.com>
2024-08-24 21:49:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+(function(){
+const fonts=["cursive", "sans-serif", "serif", "monospace"]
+let captchaValue=""
+function generateCaptcha()
+{
+    let value=btoa(Math.random()*10000000)
+    value=value.substring(0,5+Math.random()*5)
+    captchaValue=value
+}
+function setCaptcha()
+{
+    let html =captchaValue.split("").map((char)=>{
+        const rotate = -20 + Math.trunc(Math.random()*30)
+        const font=Math.trunc(Math.random()*fonts.length)
+        return `<span
+        style="transform:rotate(${rotate}deg)
+        font-family:${fonts[font]}"
+        >
+        ${char} </span>`
+    }).join("")
+    document.querySelector(".login-form .captcha .preview").innerHTML = html
+}
+function initCaptcha()
+{
+    document.querySelector(".login-form .captcha .captcha-refresh").addEventListener("click",function(){
+        generateCaptcha()
+        setCaptcha()
+    })
+    generateCaptcha()
+        setCaptcha()
+}initCaptcha()
+
+document.querySelector(".login-form #login-btn").addEventListener("click",()=>{
+    let inputCaptchaValue =document.getElementById("captcha-form").value
+    if(inputCaptchaValue===captchaValue)
+    {
+        swal("","Loggin IN!", "success")
+    }
+    else
+    {
+        swal("invalid Captcha")
+    }
+})
+
+})();
